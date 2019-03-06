bae7e402ec69e098958d2c7f1667dc7e1227ab86
Damien <damo989@iinet.net.au>
2019-03-07 07:58:16 +0800

    Added continuas function

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let min = 1
 let max = 10
+p1 = ''
+p2 = ''
+p1.textContent = ''
+p2.textContent = ''
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -11,27 +15,42 @@ function renderTick(){
     const tick = document.querySelector('#tick')
     const answerBox = document.querySelector('#answer')
     tick.style.display = 'block'
-    answerBox.style.display = 'none'
-    
-
-    
+    answerBox.style.display = 'none' 
 }
 
-const p1 = document.createElement('p')
-const p2 = document.createElement('p')
-p1.textContent = getRandomInt(min, max)
-p2.textContent = getRandomInt(min, max)
-document.querySelector('#num-1').appendChild(p1)
-document.querySelector('#num-2').appendChild(p2)
+function renderInput(){
+    const tick = document.querySelector('#tick')
+    const answerBox = document.querySelector('#answer')
+    tick.style.display = 'none'
+    answerBox.style.display = 'block'
+}
+
+let renderProblem = function(){
+    p1.textContent = ''
+    p2.textContent = ''
+    p1 = document.createElement('p')
+    p2 = document.createElement('p')
+    p1.textContent = getRandomInt(min, max)
+    p2.textContent = getRandomInt(min, max)
+    document.querySelector('#num-1').appendChild(p1)
+    document.querySelector('#num-2').appendChild(p2)
+    return(p1.textContent, p2.textContent)
+}
+
+
+renderProblem()
+
 
 
 document.querySelector('#answer').addEventListener('submit', function(e){
     e.preventDefault()
     number1 = Number(p1.textContent)
     number2 = Number(p2.textContent)
-    console.log(number1 + number2)
+    console.log('hello' + number1 + number2)
     if (number1 + number2 == e.target.elements.checkAnswer.value){
         renderTick()
+        setTimeout(renderProblem, 2000)
+        setTimeout(renderInput, 2000)
     }else{
         console.log('Wrong');
     }
