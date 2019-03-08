5ecf2a98b86f861e1d03b4a9d7f89caff34c04e2
damo989 <damo989@iinet.net.au>
2019-03-08 21:19:57 +0800

    Added minus problems

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ p1 = ''
 p2 = ''
 p1.textContent = ''
 p2.textContent = ''
+let symbol = ''
+
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -26,17 +28,34 @@ function renderInput(){
 }
 
 let renderProblem = function(){
-    p1.textContent = ''
-    p2.textContent = ''
+    symbolPicker()
+    p1.textContent = '' // Resets random number otherwise they keep concatanating 
+    p2.textContent = ''// Resets random number otherwise they keep concatanating 
     p1 = document.createElement('p')
     p2 = document.createElement('p')
+
     p1.textContent = getRandomInt(min, max)
     p2.textContent = getRandomInt(min, max)
     document.querySelector('#num-1').appendChild(p1)
     document.querySelector('#num-2').appendChild(p2)
+    document.getElementById('symbol').textContent = symbol
     
-    
-    
+}
+
+const randomOnetoTwo = function(){
+    minrn = Math.ceil(1);
+    maxrn = Math.floor(3);
+    num = Math.floor(Math.random() * (maxrn - minrn)) + minrn; //The maximum is exclusive and the minimum is inclusive
+    return num
+}
+
+function symbolPicker(){
+    num = randomOnetoTwo()
+    if (num === 1){
+        symbol = '+'
+    }else{
+        symbol = "-"
+    }
 }
 
 const focusTextBox = function() {           
@@ -47,12 +66,13 @@ renderProblem()
 
 
 
+
 document.querySelector('#answer').addEventListener('submit', function(e){
     e.preventDefault()
     number1 = Number(p1.textContent)
     number2 = Number(p2.textContent)
-    console.log('hello' + number1 + number2)
-    if (number1 + number2 == e.target.elements.checkAnswer.value){
+    correctAnswer = eval(number1 + symbol + number2)
+    if (correctAnswer == e.target.elements.checkAnswer.value){
         renderTick()
         setTimeout(renderProblem, 2000)
         setTimeout(renderInput, 2000)
---
+++ test.js
@@ -0,0 +1,9 @@
+let numbers= [5, 6]
+
+let opp = '+'
+
+//answer = eval(num1 + opp + num2)
+//console.log(answer)
+numbers.sort((a, b) => b - a);
+
+console.log(numbers)
